07c17b06a9bf8f6dff7c3237904d867a27b1d9c1
SEIGNOBOS Louis <Louis.SEIGNOBOS@akkodis.com>
2026-05-22 15:05:32 +0200

    Pydantic class test

---
+++ src/core/models.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field, conint, constr
+
+class StrictGreetRequest(BaseModel):
+    name: constr(min_length=1, max_length=50)
+
+    count: conint(ge=1, le=10) = 1
+
+    is_doctor: bool = False
+
+    title: str = Field(default="", regex=r"^(Dr\.|M\.|Mme|)$")
