e76ed8f0b9db119ba61fddc64fdaa0587191c64d
SEIGNOBOS Louis <Louis.SEIGNOBOS@akkodis.com>
2026-05-22 16:32:23 +0200

    removed min length parameter

--- src/core/models.py
+++ src/core/models.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field, conint, constr
 
 class StrictGreetRequest(BaseModel):
-    name: constr(min_length=1, max_length=50) = ""
+    name: constr(max_length=50) = ""
 
     count: conint(ge=1, le=10) = 1
 
