b61961761c4c3f57583ede8cd6ef3acdaa686955
SEIGNOBOS Louis <Louis.SEIGNOBOS@akkodis.com>
2026-05-22 16:50:16 +0200

    added min_lenght paramter to name field

--- src/core/models.py
+++ src/core/models.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field, conint, constr
 
 class StrictGreetRequest(BaseModel):
-    name: constr(max_length=50) = ""
+    name: constr(min_length=0, max_length=50) = ""
 
     count: conint(ge=1, le=10) = 1
 
