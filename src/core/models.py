c00a0e5b9651a3f14e898b6d275c7203d4b335fc
SEIGNOBOS Louis <Louis.SEIGNOBOS@akkodis.com>
2026-05-25 10:53:52 +0200

    made model abstract as a template and not business related

--- src/core/models.py
+++ src/core/models.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel, ConfigDict, Field, conint, constr
 
-class StrictGreetRequest(BaseModel):
+class StrictModelTemplate(BaseModel):
     model_config = ConfigDict(extra='forbid')
-    name: constr(min_length=0, max_length=50) = ""
-    count: conint(ge=1, le=10) = 1
-    is_doctor: bool = False
-    title: str = Field(default="", pattern=r"^(Dr\.|M\.|Mme|)$")
+    str_max_5: constr(min_length=0, max_length=5) = ""
+    int_max_5: conint(ge=1, le=5) = 1
+    bool: bool = False
+    pattern_match: str = Field(default="", pattern=r"^(Dr\.|M\.|Mme|)$")
